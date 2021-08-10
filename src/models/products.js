af620629c2e56c8359062c2df82c0d8a3e5a8cae
Juan Camilo Farfan <42941372+jcamilofarfan@users.noreply.github.com>
2021-08-09 22:34:58 -0500

    product model

---
+++ src/models/products.js
@@ -0,0 +1,9 @@
+export function product (name, price, discount, cash_discount, rate, category, category_importance){
+    this.name = name; //string
+    this.price = price; //number
+    this.discount = discount; //number
+    this.cash_discount = cash_discount; //number
+    this.rate = rate; //number
+    this.category = category; //string
+    this.category_importance = category_importance; //number
+}
